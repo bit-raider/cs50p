9a4a21a8d628fb397a0eb5a04d7a9aabab04b31d
Bit raider <297920282+bit-raider@users.noreply.github.com>
2026-09-08 23:06:14 +0530

    Broken plates.py, fix later

--- problemset2/plates.py
+++ problemset2/plates.py
@@ -1,23 +1,35 @@
 def main():
     plate = input("Plate: ").upper()
-    if is_valid(plate):
+    s = str
+    if is_valid(s):
         print("Valid")
     else:
         print("Invalid")
 
 
 def is_valid(s):
-    if len(s) <2 or len(s) > 7 :
+    if char.isdigit() or char.isalpha():
+        if len(s) < 2 or len(s) > 6 :
+            return False
+        elif not s[0, 1].isalpha():
+            return False
+        number_started = False
+        for char in s:
+            if char.isdigit():
+                number_started = True
+            elif number_started and char.isalpha():
+                return False
+    else:
         return False
-    elif not s[0, 1].isalpha():
-        return False
-    elif ... :
-        ...
-
+    
+        
 main()
 
 # conditions divided into categories, length done, first two chars be alphabet is done
 # numbers in the middle not allowed is a problem yet to figure out
+# update: done, used a killswitch and "and" is looking more interesting than I originally thought
 # along with last char being only number
+
 # and lastly no punctuation and special characters, shouldn't be that bad
+
 
