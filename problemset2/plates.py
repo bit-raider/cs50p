9c9729d0bdd639fad4db1437054f76c5f08cc1b1
Bit raider <297920282+bit-raider@users.noreply.github.com>
2026-09-11 22:26:00 +0530

    comments

--- problemset2/plates.py
+++ problemset2/plates.py
@@ -33,4 +33,6 @@ main()
 
 # and lastly no punctuation and special characters, shouldn't be that bad
 
-#did some shit and there's a million errors, 08/09/2026, cya later
+# did some shit and there's a million errors, 08/09/2026, cya later
+# did some more shit and I think it works now idk, 09/09/2026, cya
+
